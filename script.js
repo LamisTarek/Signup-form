ade99113cd872ee0e832d3185fe9cce4b9235148
LamisTarek <lameeswaheed99@gmail.com>
2023-04-13 16:49:49 +0200

    animated form

---
+++ script.js
@@ -0,0 +1,6 @@
+const container=document.querySelector(".container");
+const signupBtn=document.querySelector(".green-bg button");
+signupBtn.addEventListener("click",function(){
+    container.classList.toggle("change");
+    
+})
